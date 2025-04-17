97c95e72a3508686f34983ff7f60d578b7cf0bc7
Chand <chandbabuuu65@gmail.com>
2025-04-17 15:54:47 +0300

    Update 0_AADI-LASSI.js

--- Priyansh/commands/0_AADI-LASSI.js
+++ Priyansh/commands/0_AADI-LASSI.js
@@ -1,20 +1,20 @@
 const fs = require("fs");
 module.exports.config = {
-        name: "lassi",
+        name: "badamdrink",
     version: "1.0.1",
         hasPermssion: 0,
         credits: "AADI BABU", 
         description: "hihihihi",
         commandCategory: "no prefix",
-        usages: "lassi",
+        usages: "badamdrink",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
-        if (event.body.indexOf("lassi")==0 || event.body.indexOf("Lassi")==0 || event.body.indexOf("LASSI")==0 || event.body.indexOf("LASSEE")==0) {
+        if (event.body.indexOf("badam drink")==0 || event.body.indexOf("Badam drink")==0 || event.body.indexOf("BADAMDRINK")==0 || event.body.indexOf("Badamdrink")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐋𝐚𝐒𝐒𝐢\n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐀𝐦𝐞𝐞𝐫 𝐊𝐡𝐀𝐚𝐧 \n< ────────────────── >\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐁𝐚𝐃𝐚𝐦𝐃𝐫𝐢𝐧𝐤\n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/d190b09072f95cf62eda8634c130be8c.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
